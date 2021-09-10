82a14c76af469696d193b099cda64453499508e9
Styllth Saraiva Ribeiro <styllth@yahoo.com.br>
2021-09-10 17:07:38 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,28 @@
+const Footer = () => (
+  <footer className="footer mt-auto py-3 bg-dark">
+    <div className="container">
+      <p className="text-light">
+        Desenvolvido por:{" "}
+        <a href="https://github.com/styllth" target="_blank" rel="noreferrer">
+          Styllth Saraiva Ribeiro
+        </a>
+      </p>
+      <p className="text-light">
+        <small>
+          <strong>Semana Spring React</strong>
+          <br />
+          Evento promovido por: DevSuperior school:{" "}
+          <a
+            href="https://instagram.com/devsuperior.ig"
+            target="_blank"
+            rel="noreferrer"
+          >
+            @devsuperior.ig
+          </a>
+        </small>
+      </p>
+    </div>
+  </footer>
+);
+
+export default Footer;
